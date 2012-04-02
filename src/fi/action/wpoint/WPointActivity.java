52056c4890e1ecbb8ffde71fe3151b1f7a5c33a7
Mikko Johansson <mikko.johansson@helsinki.fi>
2012-04-02 23:43:59 +0300

    first

---
+++ src/fi/action/wpoint/WPointActivity.java
@@ -0,0 +1,22 @@
+package fi.action.wpoint;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class WPointActivity extends MapActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        // zooming
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+}
