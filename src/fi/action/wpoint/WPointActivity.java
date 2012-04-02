d291b67a96a5326738fb9cde9537b19c77c0467c
Mikko Johansson <mikko.johansson@helsinki.fi>
2012-04-03 00:53:24 +0300

    spots

--- src/fi/action/wpoint/WPointActivity.java
+++ src/fi/action/wpoint/WPointActivity.java
@@ -1,18 +1,41 @@
 package fi.action.wpoint;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class WPointActivity extends MapActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // layout
         setContentView(R.layout.main);
+        
         // zooming
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+        // spots
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        Spot spotOverlay = new Spot(drawable, this);
+        
+        // test
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+        
+        // load spots
+        spotOverlay.add(overlayitem);
+        mapOverlays.add(spotOverlay);
+        
     }
     
     @Override
